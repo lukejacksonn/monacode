f83ba8caff3617bb80f66b52516554f48b4f87c8
lukejacksonn <lukejacksonn@gmail.com>
2020-07-30 00:17:54 +0100

    Remove all css imports

--- esm/vs/base/browser/ui/codiconLabel/codiconLabel.js
+++ esm/vs/base/browser/ui/codiconLabel/codiconLabel.js
@@ -2,5 +2,5 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import './codicon/codicon.css';
-import './codicon/codicon-animations.css';
+// import './codicon/codicon.css';
+// import './codicon/codicon-animations.css';
